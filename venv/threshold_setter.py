7fa515eebb498e54ae26f2729cfd9d8cc2f25a13
graham.williams <graham.andrew.williams@gmail.com>
2023-04-27 13:44:28 -0500

    Added discrepancy_finder.py.

--- venv/threshold_setter.py
+++ venv/threshold_setter.py
@@ -1,63 +1,60 @@
 import tkinter as tk
-from tkinter import ttk
-import os
-import time
+from tkinter import messagebox
 import core
 
-def update_nominal_label():
-    num_nominal_photos = len(nominal_images)
-    nominal_label_text.set(f"Nominal photos: {num_nominal_photos}\nDifferences: {nominal_differences}")
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+        self.nominal_images = []
+        self.discrepancy_images = []
 
-def update_discrepancy_label():
-    num_discrepancy_photos = len(discrepancy_differences)
-    discrepancy_label_text.set(f"Discrepancy photos: {num_discrepancy_photos}\nDifferences: {discrepancy_differences}")
+    def create_widgets(self):
+        self.nominal_button = tk.Button(self)
+        self.nominal_button["text"] = "Nominal"
+        self.nominal_button["command"] = self.capture_nominal
+        self.nominal_button.place(x=100, y=200, width=200, height=100)
 
-def on_nominal_button_click():
-    image_name = "nominal_{}.jpg".format(time.time())
-    core.capture_photo(image_name)
-    nominal_images.append(image_name)
-    if len(nominal_images) > 1:
-        difference = core.compare_images(nominal_images[-2], nominal_images[-1])
-        nominal_differences.append(difference)
-    update_nominal_label()
+        self.discrepancy_button = tk.Button(self)
+        self.discrepancy_button["text"] = "Discrepancy"
+        self.discrepancy_button["command"] = self.capture_discrepancy
+        self.discrepancy_button.place(x=500, y=200, width=200, height=100)
 
-def on_discrepancy_button_click():
-    image_name = "discrepancy_{}.jpg".format(time.time())
-    core.capture_photo(image_name)
-    difference = min(core.compare_images(nominal_image, image_name) for nominal_image in nominal_images)
-    discrepancy_differences.append(difference)
-    update_discrepancy_label()
+        self.reset_button = tk.Button(self)
+        self.reset_button["text"] = "Reset"
+        self.reset_button["command"] = self.reset
+        self.reset_button.place(x=350, y=400, width=100, height=50)
 
-def on_reset_button_click():
-    global nominal_images, nominal_differences, discrepancy_differences
-    nominal_images = []
-    nominal_differences = []
-    discrepancy_differences = []
-    update_nominal_label()
-    update_discrepancy_label()
+        self.nominal_label = tk.Label(self)
+        self.nominal_label.place(x=100, y=350)
+
+        self.discrepancy_label = tk.Label(self)
+        self.discrepancy_label.place(x=500, y=350)
+
+    def update_labels(self):
+        nominal_diff = core.get_difference(self.nominal_images) if self.nominal_images else 0
+        discrepancy_diff = core.get_difference(self.discrepancy_images) if self.discrepancy_images else 0
+        discrepancy_range = core.get_difference_range(self.nominal_images, self.discrepancy_images)
+
+        self.nominal_label["text"] = f"Nominal images: {len(self.nominal_images)}, difference: {nominal_diff}"
+        self.discrepancy_label["text"] = f"Discrepancy images: {len(self.discrepancy_images)}, difference: {discrepancy_diff}, range: {discrepancy_range}"
+
+    def capture_nominal(self):
+        self.nominal_images.append(core.capture_photo())
+        self.update_labels()
+
+    def capture_discrepancy(self):
+        self.discrepancy_images.append(core.capture_photo())
+        self.update_labels()
+
+    def reset(self):
+        self.nominal_images = []
+        self.discrepancy_images = []
+        self.update_labels()
 
 root = tk.Tk()
 root.geometry("800x480")
-
-nominal_images = []
-nominal_differences = []
-discrepancy_differences = []
-
-nominal_label_text = tk.StringVar()
-nominal_label = tk.Label(root, textvariable=nominal_label_text)
-nominal_label.place(x=20, y=90)
-
-discrepancy_label_text = tk.StringVar()
-discrepancy_label = tk.Label(root, textvariable=discrepancy_label_text)
-discrepancy_label.place(x=420, y=90)
-
-nominal_button = tk.Button(root, text="Nominal", command=on_nominal_button_click, width=20, height=5)
-nominal_button.place(x=20, y=20)
-
-discrepancy_button = tk.Button(root, text="Discrepancy", command=on_discrepancy_button_click, width=20, height=5)
-discrepancy_button.place(x=420, y=20)
-
-reset_button = tk.Button(root, text="Reset", command=on_reset_button_click, width=10, height=2)
-reset_button.place(x=700, y=400)
-
-root.mainloop()
+app = Application(master=root)
+app.mainloop()
